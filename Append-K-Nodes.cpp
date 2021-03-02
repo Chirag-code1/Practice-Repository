d5ba3e2f150366832d7344497e588c67d32e0cf0
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-02 23:01:06 +0530

    Add files via upload

---
+++ Append-K-Nodes.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val){
+    node* n= new node(val);
+    n->next=head;
+    head=n;
+    
+}
+
+void insertAtTail(node* &head, int val){
+    node* n= new node(val);
+    
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    
+    node* temp=head;
+    
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+    
+}
+
+void display(node* head){
+    while(head!=NULL){
+       // head=head->next;
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+
+bool search(node* head, int key){
+    node* temp=head;
+    while(temp!=NULL){
+        if(temp->data==key){
+            return true;
+        }
+        temp=temp->next;
+    }
+    return false;
+}
+
+int length(node* head){
+    int l=0;
+    node* temp=head;
+    while(temp!=NULL){
+        l++;
+        temp=temp->next;
+    }
+    return l;
+}
+
+node* kappend(node* &head, int k){ // here, k is the node number from where linked list will get appended.
+    
+    node* newHead;
+    node* newTail;
+    node* tail=head;
+    int l=length(head);
+    k=k%l;
+    int count=1;
+    while(tail->next!=NULL){
+        if(count==l-k){
+            newTail=tail;
+        }
+        if(count==l-k+1){
+            newHead=tail;
+        }
+        tail=tail->next;
+        count++;
+    }
+    
+    newTail->next=NULL;
+    tail->next=head;
+    
+    return newHead;
+    
+}
+
+int main(){
+    node* head=NULL;
+    insertAtHead(head,10);
+    insertAtTail(head,20);
+    insertAtTail(head,30);
+    insertAtTail(head,40);
+    insertAtTail(head,50);
+    insertAtTail(head,60);
+    display(head); 
+    node* newHead=kappend(head,3);
+    display(newHead);
+ 
+    return 0;
+}
