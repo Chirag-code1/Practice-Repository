171f3a59e161b759f62b9acb1b7bb425c899853f
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-15 23:39:09 +0530

    Add files via upload

---
+++ min-no-of-coins-MEMOIZATION.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=18;
+	    int a[]= {7,5,1};
+	    
+	    int dp[]= new int[n+1];
+	   
+	    Arrays.fill(dp,-1);
+	  //  dp[]={-1};
+	    dp[0]=0;
+    
+		System.out.println(mincoins(n,a,dp));
+	}
+
+
+
+static int mincoins(int n, int a[],int dp[]){
+    
+    if(n == 0) return 0;
+    int ans= Integer.MAX_VALUE;
+    
+    for(int i=0;i<a.length;i++){
+        if(n-a[i]>=0){
+            int subans=0;
+            if(dp[n-a[i]]!=-1){
+                subans= dp[n-a[i]];
+            }
+            else{
+         subans= mincoins(n-a[i],a,dp);
+            }//end of else.
+            if(subans!= Integer.MAX_VALUE && subans+1<ans){
+                ans=subans+1;
+            }
+        }
+       
+    }
+    return dp[n]=ans;
+}
+
+}
