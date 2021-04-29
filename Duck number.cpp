7fd59d34e3c262d72724be40956039c166252530
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-04-29 23:48:07 +0530

    Add files via upload

---
+++ Duck number.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+bool check_duck(string num)
+{
+	int i = 0, n = num.length();
+	while (i < n && num[i] == '0')
+		i++;
+
+
+	while (i < n) {
+		if (num[i] == '0')
+			return true;
+		i++;
+	}
+
+	return false;
+}
+
+int main(void)
+{
+	string num = "1023";
+	if (check_duck(num))
+		cout << "It is a duck number\n";
+	else
+		cout << "It is not a duck number\n";
+
+	return 0;
+}
